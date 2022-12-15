d6a925a9846978cd73d32b90dc03d799ef51a16d
Fabio <fpreviatti@hotmail.com>
2022-12-15 14:27:59 -0300

    exemplo admin=true

--- teste-sql-injection-frontend/teste-sql-injection/src/pages/login.js
+++ teste-sql-injection-frontend/teste-sql-injection/src/pages/login.js
@@ -37,7 +37,7 @@ export default class Login extends Component {
           },
           resolve
         )
-      );
+      )
 
       if (this.state.toDashboard) {
         console.log(response);
--- teste-sql-injection/src/main/java/com/testesqlinjection/repository/UserRepository.java
+++ teste-sql-injection/src/main/java/com/testesqlinjection/repository/UserRepository.java
@@ -3,16 +3,11 @@ package com.testesqlinjection.repository;
 import com.testesqlinjection.model.UserInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-
+import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends JpaRepository<UserInfo,Long> {
 
-
-    //UserInfo findByEmail(String email);
-
-
-    @Query(value = "select * from user_info where email = ?1", nativeQuery = true)
-    UserInfo findByEmail(String email);
-
+    @Query(value = "select * from user_info where email = :email or admin = true" , nativeQuery = true)
+    UserInfo findByEmail( @Param("email") String email);
 
 }
